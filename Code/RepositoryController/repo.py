fdf4dc9adead09e65c271fabc5628c397beec7b3
Phan Minh Anh Tuan <pmat@MacBook-Pro-2.local>
2023-01-11 08:42:14 +0700

    Update merge full

--- Code/RepositoryController/repo.py
+++ Code/RepositoryController/repo.py
@@ -1,4 +1,5 @@
 import gitlab
+import asyncio
 
 from Login import login
 
@@ -8,51 +9,53 @@ class RepositoryController:
         self.gl = login(token)
 
     # Xem danh sách repo
-    def listRepositories(self):
+    async def listRepositories(self):
         repos = self.gl.projects.list(owned=True)
         return repos
 
     # Tạo mới repo
-    def createRepository(self, name, visibility):
+    async def createRepository(self, name, visibility):
         new_repo = self.gl.projects.create({'name': name, 'visibility': visibility})
         return new_repo
 
     # Xóa repo
-    def deleteRepository(self, name):
+    async def deleteRepository(self, name):
         repo = self.gl.projects.list(search=name)[0]
         self.gl.projects.delete(repo.id)
 
     # Liệt kê danh sách branch
-    def listBranches(self, repo_name):
+    async def listBranches(self, repo_name):
         repo = self.gl.projects.list(search=repo_name)[0]
         # Trả về toàn bộ branch
         branches = repo.branches.list()
         return branches
 
     # Merge branch
-    def mergeBranchRequest(self, repo_name, branch_name):
+    async def mergeBranchRequest(self, repo_name, source_branch, target_branch):
         repo = self.gl.projects.list(search=repo_name)[0]
         # check for open merge requests for the same source and target branches
-        existing_merge_requests = repo.mergerequests.list(source_branch=branch_name, target_branch='main',
+        existing_merge_requests = repo.mergerequests.list(source_branch=source_branch, target_branch=target_branch,
                                                           state='opened')
+        merge_request = None
         if existing_merge_requests:
-            print(f"A merge request for source branch '{branch_name}' and target branch 'main' already exists.")
-            return 0
+            print(f"A merge request for source branch '{source_branch}' and target branch '{target_branch}' already exists.")
         else:
             # Create a merge request
             merge_request = repo.mergerequests.create({
-                'source_branch': branch_name,
-                'target_branch': 'main',
-                'title': f'Merging \'{branch_name}\' into main'
+                'source_branch': source_branch,
+                'target_branch': target_branch,
+                'title': f'Merging \'{source_branch}\' into \'{target_branch}\''
             })
-            return merge_request
+            if merge_request.changes()['merge_status'] != 'can_be_merged':
+                print(f"A merge request for source branch '{source_branch}' and target branch '{target_branch}' cannot be merged")
+                merge_request.delete()
+                merge_request = None
+        return merge_request
 
-    def acceptRequest(self, repo_name='NMCNPM.20221-Nhom4', merge_request_id=198511948):
-        repo = self.gl.projects.list(search=repo_name)[0]
-        merge_request = repo.mergerequests.get(merge_request_id)
+    async def acceptRequest(self, merge_request):
         merge_request.merge()
 
-    def declineRequest(self, repo_name='NMCNPM.20221-Nhom4', merge_request_id=0):
+    async def declineRequest(self, repo_name, merge_request_id):
         # Get the project by name
         project = self.gl.projects.list(search=repo_name)[0]
 
--- Code/test.py
+++ Code/test.py
@@ -4,8 +4,17 @@ import asyncio
 from RepositoryController.repo import RepositoryController
 import gitlab
 
-if __name__ == '__main__':
+
+async def main():
     repo_controller = RepositoryController('glpat-A4wxGBzLAgBxyYBueyS6')
-    merge_request = repo_controller.mergeBranchRequest('NMCNPM.20221-Nhom4', 'pmatuan')
-    time.sleep(3)
-    merge_request.merge()
+    merge_request = await repo_controller.mergeBranchRequest('ProjectOOP', 'pmatuan', 'master')
+    await asyncio.sleep(3)
+    if merge_request is not None:
+        if merge_request.changes()['merge_status'] == 'can_be_merged':
+            await repo_controller.acceptRequest(merge_request)
+        else:
+            print('Cannot merge')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
